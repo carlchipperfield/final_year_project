carlchipperfield/final_year_project

commit 616d56f9fe14d5728241d4b8a69eaba805cae9f7

#16 Reorganised web files; split partials into directories feature based. combined all library files into lib/. split snapshot_controllers into separate files for service, controllers, filters, directives and app. Finally, restructed the grunt build file.

main/grunt.js
@@ -1,18 +1,19 @@
 /*global module:false*/
 module.exports = function (grunt) {
 
-    var SRC_CSS    = 'web/css/',
-        SRC_JS     = 'web/js/',
-        SRC_JS_LIB = SRC_JS + 'libraries/';
-
     // Project configuration.
     grunt.initConfig({
+
+        build_dirs: {
+            api: 'build/app/share/api',
+            web: 'build/app/share/web',
+            python: 'build/app/share/python'
+        },
 
         lint: {
             files: [
                 'grunt.js',
-                SRC_JS + '*.js',
-                SRC_JS + 'utils/*.js'
+                'web/js/**'
             ]
         },
 
@@ -21,37 +22,54 @@ module.exports = function (grunt) {
 
             js: {
                 files: {
-
-                    'build/app/share/web/js/libraries/jquery-1.8.2.min.js'
-                        : SRC_JS_LIB + 'jquery-1.8.2.min.js',
-
-                    'build/app/share/web/js/libraries/angular.min.js'
-                        : SRC_JS_LIB + 'angular.min.js',
-
-                    'build/app/share/web/js/libraries/bootstrap.min.js'
-                        : 'web/bootstrap/js/bootstrap.min.js',
-
-                    'build/app/share/web/js/source/core/snapshot_upload.js'
-                        : SRC_JS + 'core/snapshot_upload.js',
-
-                    'build/app/share/web/js/source/core/snapshots_controller.js'
-                        : SRC_JS + 'core/snapshots_controller.js'
+                    '<%=build_dirs.web%>/js/source/': [
+                        'web/js/app.js',
+                        'web/js/filters.js',
+                        'web/js/directives.js',
+                        'web/js/services.js',
+                        'web/js/controllers.js'
+                    ]
                 }
             },
 
             css: {
                 files: {
-                    'build/app/share/web/css/' : 'web/css/normalize.css',
-                    'build/app/share/web/bootstrap/' : 'web/bootstrap/**'
+                    '<%=build_dirs.web%>/css/': [
+                        'web/css/normalize.css'
+                    ]
+                }
+            },
+
+            templates: {
+                files: {
+                    '<%=build_dirs.web%>/': [
+                        'web/index.html',
+                        'web/partials/**'
+                    ]
+                }
+            },
+
+            lib: {
+                files: {
+                    '<%=build_dirs.web%>/lib/': [
+                        'web/lib/angular/angular.min.js',
+                        'web/lib/angular/angular-resource.min.js',
+                        'web/lib/jquery/jquery-1.8.2.min.js',
+                        'web/lib/bootstrap/css/bootstrap.min.css',
+                        'web/lib/bootstrap/js/bootstrap.min.js',
+                        'web/lib/bootstrap/img/**'
+                    ]
                 }
             },
 
             core: {
                 files: {
-                    'build/app/share/api/': 'api/**',
-                    'build/app/share/python/site-packages/ni/': 'functional/ni/**',
-                    'build/app/share/web/': 'web/*',
-                    'build/app/share/web/partials/': 'web/partials/*',
+                    // Copy the API
+                    '<%=build_dirs.api%>/': 'api/**',
+
+                    // Copy the functional python source
+                    '<%=build_dirs.python%>/site-packages/ni/'
+                        : 'functional/ni/**',
 
                     // Copy the python virtualenv to the build
                     'build/app/python/': 'python/**'
@@ -72,7 +90,7 @@ module.exports = function (grunt) {
             core: {
 
                 src: [
-                    'source/core/*.js'
+                    'source/*.js'
                 ],
                 dest: 'core.min.js',
                 destMap: 'core.js.map',
@@ -84,8 +102,8 @@ module.exports = function (grunt) {
             core: {
                 files: {
                     'build/app/share/web/css/core.css':  [
-                        SRC_CSS + 'main_layout.scss',
-                        SRC_CSS + 'navigation.scss'
+                        'web/css/main_layout.scss',
+                        'web/css/navigation.scss'
                     ]
                 }
             }
@@ -126,7 +144,8 @@ module.exports = function (grunt) {
             globals: {
                 jQuery : true,
                 'alert': true,
-                'console': true
+                'console': true,
+                'angular': true
             }
         },
 

main/web/index.html
@@ -7,7 +7,7 @@
     <title>Network Traffic</title>
 
     <link rel="stylesheet" type="text/css" href="/css/normalize.css">
-    <link rel="stylesheet" type="text/css" href="/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/lib/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="/css/core.css">
 </head>
 
@@ -15,17 +15,17 @@
     <div id="container" ng-app="app" ng-controller="AppCtrl">
 
         <header>
-            <div class="navbar" ng-include="'/partials/navigation.html'"></div>
-            <div ng-include="'/partials/apptoolbar.html'"></div>
+            <div class="navbar" ng-include="'/partials/core/navigation.html'"></div>
+            <div ng-include="'/partials/core/apptoolbar.html'"></div>
         </header>
 
         <div id="content-wrapper" ng-view></div>
     </div>
 
-    <script src="/js/libraries/jquery-1.8.2.min.js"></script>
-    <script src="/js/libraries/bootstrap.min.js"></script>
-    <script src="/js/libraries/angular.min.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.0.3/angular-resource.min.js"></script>
+    <script src="/lib/jquery/jquery-1.8.2.min.js"></script>
+    <script src="/lib/bootstrap/js/bootstrap.min.js"></script>
+    <script src="/lib/angular/angular.min.js"></script>
+    <script src="/lib/angular/angular-resource.min.js"></script>
     <script src="/js/core.min.js"></script>
 
 </html>

main/web/js/app.js
@@ -1,0 +1,27 @@
+var modules = [
+    'app.filters',
+    'app.directives',
+    'app.services',
+    'app.controllers',
+    'ngResource'
+];
+
+// Declare app level module
+angular.module('app', modules)
+
+.config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
+
+    var snapshot_data = {
+        templateUrl: '/partials/networktraffic/index.html',
+        reloadOnSearch: false
+    };
+
+    var upload_data = {
+        templateUrl: '/partials/networktraffic/snapshotupload.html'
+    };
+
+    $routeProvider.when('/networktraffic', snapshot_data)
+                  .when('/networktraffic/upload', upload_data)
+                  .otherwise({redirectTo: '/networktraffic'});
+});

main/web/js/controllers.js
@@ -4,104 +4,21 @@ var actions = {
     remove: {method: 'DELETE'}
 };
 
-var app = angular.module('app', ['ngResource']);
-
-app.config(function($routeProvider, $locationProvider){
-    $locationProvider.html5Mode(true);
-
-    var snapshot_data = {
-        templateUrl: '/partials/networktraffic.html',
-        reloadOnSearch: false
-    };
-
-    var upload_data = {
-        templateUrl: '/partials/snapshotupload.html'
-    };
-
-    $routeProvider.when('/networktraffic', snapshot_data)
-                  .when('/networktraffic/upload', upload_data)
-                  .otherwise({redirectTo: '/networktraffic'});
-});
-
-app.filter('formatdate', function() {
-    return function(input) {
-        var date = new Date(input);
-        return date.toLocaleDateString() + ' ' + date.toLocaleTimeString();
-    };
-});
-
-app.filter('formatport', function() {
-    return function(input) {
-        if (input !== '') {
-            return ':' + input;
-        }
-        return input;
-    };
-});
-
-app.filter('capitalize', function() {
-    return function(input) {
-        if (!input) {
-            return input;
-        }
-        return input.charAt(0).toUpperCase() + input.slice(1);
-    };
-});
-
-/* Diective to resize the content wrapper */
-app.directive('resize', function($timeout) {
-
-    var directiveDefinitionObject = {
-        compile: function compile() {
-            return function postLink() {
-                var header = $('header');
-                var container = $('#content-wrapper');
-
-                function resizeWrapper() {
-                    container.height($(window).height() - header.height());
-                }
-                resizeWrapper();
-
-                $(window).resize( function() {
-                    resizeWrapper();
-                });
-            };
-        }
-    };
-    return directiveDefinitionObject;
-});
+angular.module('app.controllers', [])
 
 
-app.factory('mySharedService', function($rootScope) {
 
-    var sharedService = {};
-    this.snapshot = undefined;
-
-    sharedService.displaySnapshot = function(snapshot) {
-        this.snapshot = snapshot;
-        $rootScope.$broadcast('displaysnapshot');
-    };
-
-    sharedService.removeSnapshot = function() {
-        $rootScope.$broadcast('removesnapshot');
-    };
-
-    return sharedService;
-});
+.controller('AppCtrl', function ($scope) {
+    $scope.page = {};
+})
 
 
-function AppCtrl($scope, mySharedService) {
-    $scope.page = {
-        
-    };
-}
 
-
-function SnapshotListCtrl($scope, $resource, mySharedService, $location, $routeParams, $window)
+.controller('SnapshotListCtrl', function ($scope, $resource, mySharedService, $location, $routeParams, $window)
 {
     var Snapshot = $resource('/api/snapshot/:id', {}, actions);
 
-    this.process_snapshots = function() {
+    this.process_snapshots = function () {
         var snapshot = {};
 
         if ($routeParams.id) {
@@ -124,17 +41,17 @@ function SnapshotListCtrl($scope, $resource, mySharedService, $location, $routeP
         mySharedService.displaySnapshot(snapshot);
     };
 
-    $scope.display_snapshot = function(index) {
+    $scope.display_snapshot = function (index) {
         var snapshot = $scope.snapshots[index];
         $location.search({id: snapshot._id});
         mySharedService.displaySnapshot(snapshot);
     };
 
-    $scope.upload = function() {
+    $scope.upload = function () {
         $window.location.href = '/#/networktraffic/upload';
     };
 
-    $scope.$on('removesnapshot', function() {
+    $scope.$on('removesnapshot', function () {
         var snapshot_id = mySharedService.snapshot._id;
 
         for (var i = 0; i < $scope.snapshots.length; i++) {
@@ -157,10 +74,11 @@ function SnapshotListCtrl($scope, $resource, mySharedService, $location, $routeP
     });
 
     $scope.snapshots = Snapshot.list({}, this.process_snapshots);
-}
+})
 
 
-function SnapshotCtrl($rootScope, $scope, $log, $resource, mySharedService)
+
+.controller('SnapshotCtrl', function ($rootScope, $scope, $log, $resource, mySharedService)
 {
     /*
         Work out how to document javascript!
@@ -182,48 +100,48 @@ function SnapshotCtrl($rootScope, $scope, $log, $resource, mySharedService)
     var Snapshot = $resource('/api/snapshot/:id', {}, actions);
     var Messages = $resource('/api/snapshot/:id/networkmessages?limit=:limit', {}, actions);
 
-    $scope.$on('displaysnapshot', function() {
+    $scope.$on('displaysnapshot', function () {
         $scope.snapshot = mySharedService.snapshot;
         if ($scope.isSnapshot()) {
             $scope.retrieve_messages($scope.snapshot._id);
         }
     });
 
-    $scope.remove = function() {
+    $scope.remove = function () {
         var success = function () {
             mySharedService.removeSnapshot();
         };
-        var fail = function() {
+        var fail = function () {
             $log.error('Delete Failed');
         };
         Snapshot.remove({id: $scope.snapshot._id}, success, fail);
     };
 
-    $scope.retrieve_messages = function(snapshot_id) {
+    $scope.retrieve_messages = function (snapshot_id) {
         var url_params = {id: snapshot_id, limit: 30};
         $scope.networkmessages = Messages.list(url_params);
     };
 
-    $scope.isSnapshot = function() {
+    $scope.isSnapshot = function () {
         var snapshot = mySharedService.snapshot;
         return snapshot !== undefined && snapshot._id !== undefined;
     };
 
-    $scope.isNoSnapshot = function() {
+    $scope.isNoSnapshot = function () {
         var snapshot = mySharedService.snapshot;
         return snapshot !== undefined && snapshot._id === undefined;
     };
-}
+})
 
 
 
-function UploadSnapshotCtrl($rootScope, $scope, $resource, $window)
+.controller('UploadSnapshotCtrl', function ($rootScope, $scope, $resource, $window)
 {
     $rootScope.page = {
         title: 'Network Traffic Upload'
     };
 
-    $scope.upload = function() {
+    $scope.upload = function () {
 
         var formData = new FormData();
         formData.append('title', $scope.title);
@@ -236,14 +154,14 @@ function UploadSnapshotCtrl($rootScope, $scope, $resource, $window)
 
         // Listen to the upload progress.
         var progressBar = document.querySelector('progress');
-        xhr.upload.onprogress = function(e) {
-          if (e.lengthComputable) {
-            progressBar.value = (e.loaded / e.total) * 100;
-            progressBar.textContent = progressBar.value;
-          }
+        xhr.upload.onprogress = function (e) {
+            if (e.lengthComputable) {
+                progressBar.value = (e.loaded / e.total) * 100;
+                progressBar.textContent = progressBar.value;
+            }
         };
 
-        xhr.onload = function() {
+        xhr.onload = function () {
             if (this.status === 200) {
                 var url = '/#/networktraffic?id=' + this.responseText;
                 $window.location.href = url;
@@ -253,10 +171,10 @@ function UploadSnapshotCtrl($rootScope, $scope, $resource, $window)
         xhr.send(formData);
     };
 
-    $scope.setFile = function(element) {
-        $scope.$apply(function($scope) {
+    $scope.setFile = function (element) {
+        $scope.$apply(function ($scope) {
             $scope.filename = element.files[0].name;
             $scope.logfile = element.files[0];
         });
     };
-}
+});

main/web/js/core/snapshot_upload.js
@@ -1,35 +1,0 @@
-function SnapshotUploadCtrl($scope) {
-
-    $scope.snapshotuploads = [
-    ];
-   
-    $scope.uploadSnapshot = function() {
-
-        var formData = new FormData();
-        formData.append('title', $scope.title);
-        formData.append('description', $scope.desc);
-        formData.append('logfile_name', $scope.filename);
-        formData.append('logfile_content', $scope.logfile);
-
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', '/api/snapshotupload', true);
-
-        // Listen to the upload progress.
-        var progressBar = document.querySelector('progress');
-        xhr.upload.onprogress = function(e) {
-          if (e.lengthComputable) {
-            progressBar.value = (e.loaded / e.total) * 100;
-            progressBar.textContent = progressBar.value; // Fallback for unsupported browsers.
-          }
-        };
-
-        xhr.send(formData);
-    };
-
-    $scope.setFile = function(element) {
-        $scope.$apply(function($scope) {
-            $scope.filename = element.files[0].name;
-            $scope.logfile = element.files[0];
-        });
-    };
-}

main/web/js/directives.js
@@ -1,0 +1,22 @@
+angular.module('app.directives', [])
+
+.directive('resize', function () {
+    var directiveDefinitionObject = {
+        compile: function compile() {
+            return function postLink() {
+                var header = $('header');
+                var container = $('#content-wrapper');
+
+                function resizeWrapper() {
+                    container.height($(window).height() - header.height());
+                }
+                resizeWrapper();
+
+                $(window).resize(function () {
+                    resizeWrapper();
+                });
+            };
+        }
+    };
+    return directiveDefinitionObject;
+});

main/web/js/filters.js
@@ -1,0 +1,29 @@
+angular.module('app.filters', [])
+
+.filter('formatdate', function () {
+    return function (input) {
+        if (input !== '') {
+            var date = new Date(input);
+            return date.toLocaleDateString() + ' ' + date.toLocaleTimeString();
+        }
+        return '-';
+    };
+})
+
+.filter('formatport', function () {
+    return function (input) {
+        if (input !== '') {
+            return ':' + input;
+        }
+        return input;
+    };
+})
+
+.filter('capitalize', function () {
+    return function (input) {
+        if (!input) {
+            return input;
+        }
+        return input.charAt(0).toUpperCase() + input.slice(1);
+    };
+});

main/web/js/services.js
@@ -1,0 +1,19 @@
+
+angular.module('app.services', [])
+
+.factory('mySharedService', function ($rootScope) {
+
+    var sharedService = {};
+    this.snapshot = undefined;
+
+    sharedService.displaySnapshot = function (snapshot) {
+        this.snapshot = snapshot;
+        $rootScope.$broadcast('displaysnapshot');
+    };
+
+    sharedService.removeSnapshot = function () {
+        $rootScope.$broadcast('removesnapshot');
+    };
+
+    return sharedService;
+});

commit a7e7f4b6f773581b84725fc5951efb247ebbeded

#16 commit 616d56f9fe caused the build process to take too long due to the new compress feature. Have removed compress from the default build and have created a new build for deployment, which also undertakes this compress stage.

main/grunt.js
@@ -62,6 +62,14 @@ module.exports = function (grunt) {
                 }
             },
 
+            img: {
+                files: {
+                    '<%=build_dirs.web%>/img/': [
+                        'web/images/cisco.png'
+                    ]
+                }
+            },
+
             core: {
                 files: {
                     // Copy the API
@@ -165,5 +173,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-jsmin-sourcemap');
 
     // Specify tasks
-    grunt.registerTask('default', 'lint copy jsmin-sourcemap sass compress');
+    grunt.registerTask('default', 'lint copy jsmin-sourcemap sass');
+    grunt.registerTask('deploy', 'lint copy jsmin-sourcemap sass compress');
 };